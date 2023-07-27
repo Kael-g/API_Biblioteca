73efd98fcd73952bb6f7a7f3ecb1a4a6257cb6dd
Kael-g <mikael.gualdi@zup.com.br>
2023-07-27 20:41:00 -0300

    Ainda sem conteúdo

---
+++ src/main/java/com/catalisa/exercicioApiBiblioteca/ExercicioApiBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.catalisa.exercicioApiBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioApiBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioApiBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalisa/exercicioApiBiblioteca/ExercicioApiBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalisa.exercicioApiBiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioApiBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
